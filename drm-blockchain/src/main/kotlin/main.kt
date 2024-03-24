4c8d9cb01f8a81f223b17d5602c72e9c0c4d4545
Smile_slime_47 <Smile_slime_47@outlook.com>
2024-03-24 14:32:14 +0800

    [blockchain]完善加密类功能

--- drm-blockchain/src/main/kotlin/main.kt
+++ drm-blockchain/src/main/kotlin/main.kt
@@ -1,42 +1,50 @@
 package moe._47saikyo
 
-import moe._47saikyo.contract.DRManager
-import moe._47saikyo.contract.Right
-import org.web3j.tx.gas.DefaultGasProvider
-import java.math.BigInteger
+import moe._47saikyo.utils.CryptoUtils
+import java.util.Base64
+import javax.crypto.spec.SecretKeySpec
 
 fun main(){
-    BlockChain.connect(
-        socket = "http://192.168.10.108:8545",
-        walletSource = "/home/smile_slime_47/Projekt/Digital-Rights-Management/drm-backend/src/main/resources/KeyStore/UTC--2024-03-16T14-15-01.824378618Z--cb7f6d5c8f5c71c3f604f6fec874a97007dfe4fe.json",
-        password = "1234567890",
-        chainId = 721
-    )
-    val addr = DRManager.deploy(
-        BlockChain.web3jInstance,
-        BlockChain.bankTxManager,
-        DefaultGasProvider()
-    ).send().contractAddress
-    val manager = DRManager.load(
-        addr,
-        BlockChain.web3jInstance,
-        BlockChain.bankTxManager,
-        DefaultGasProvider()
-    )
-    val rightAddr = Right.deploy(
-        BlockChain.web3jInstance,
-        BlockChain.bankTxManager,
-        DefaultGasProvider(),
-        BigInteger.valueOf(123),
-        "456"
-    ).send().contractAddress
-    println(rightAddr)
-    manager.addRight(rightAddr).send()
-    val right = Right.load(
-        manager.getRight("123").send(),
-        BlockChain.web3jInstance,
-        BlockChain.bankTxManager,
-        DefaultGasProvider()
-    )
-    println(right.rightName.send())
+    val iv = CryptoUtils.getRandomByteArray(CryptoUtils.IV_128_BIT)
+    val spec = CryptoUtils.getSecretKey("password")
+
+    val encrypted = CryptoUtils.encrypt("testdata".toByteArray(),spec,iv)
+    println(String(encrypted))
+
+    val decrypted = CryptoUtils.decrypt(encrypted,spec,iv)
+    println(String(decrypted))
+
+//    BlockChain.connect(
+//        socket = "http://192.168.10.108:8545",
+//        walletSource = "/home/smile_slime_47/Projekt/Digital-Rights-Management/drm-backend/src/main/resources/KeyStore/UTC--2024-03-16T14-15-01.824378618Z--cb7f6d5c8f5c71c3f604f6fec874a97007dfe4fe.json",
+//        password = "1234567890",
+//        chainId = 721
+//    )
+//    val addr = DRManager.deploy(
+//        BlockChain.web3jInstance,
+//        BlockChain.bankTxManager,
+//        DefaultGasProvider()
+//    ).send().contractAddress
+//    val manager = DRManager.load(
+//        addr,
+//        BlockChain.web3jInstance,
+//        BlockChain.bankTxManager,
+//        DefaultGasProvider()
+//    )
+//    val rightAddr = Right.deploy(
+//        BlockChain.web3jInstance,
+//        BlockChain.bankTxManager,
+//        DefaultGasProvider(),
+//        BigInteger.valueOf(123),
+//        "456"
+//    ).send().contractAddress
+//    println(rightAddr)
+//    manager.addRight(rightAddr).send()
+//    val right = Right.load(
+//        manager.getRight("123").send(),
+//        BlockChain.web3jInstance,
+//        BlockChain.bankTxManager,
+//        DefaultGasProvider()
+//    )
+//    println(right.rightName.send())
 }
--- drm-blockchain/src/main/kotlin/utils/CryptoUtils.kt
+++ drm-blockchain/src/main/kotlin/utils/CryptoUtils.kt
@@ -3,7 +3,11 @@ package moe._47saikyo.utils
 import java.security.SecureRandom
 import javax.crypto.Cipher
 import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
+
 
 /**
  * 加密工具类
@@ -11,18 +15,37 @@ import javax.crypto.spec.IvParameterSpec
  * @author 刘一邦
  */
 object CryptoUtils {
+    //128比特初始化向量
+    const val IV_128_BIT = 16
+
     //随机数生成类
     private val random = SecureRandom()
 
     /**
-     * 获取一个随机的128位初始化向量
+     * 获取一个随机的初始化向量
      *
+     * @param len 字节长度，16则对应128位
      * @return 由SecureRandom类生成的128位随机数组
      */
-    fun getRandomIv(): ByteArray {
-        val iv = ByteArray(16)
-        random.nextBytes(iv)
-        return iv
+    fun getRandomByteArray(len: Int): ByteArray {
+        val arr = ByteArray(len)
+
+        return random.nextBytes(arr).let { arr }
+    }
+
+    /**
+     * 通过PBKDF2算法将密码短语转换为256比特长度的密钥
+     *
+     * @param phrase 密码短语
+     * @return SecretKey对象
+     */
+    fun getSecretKey(phrase: String): SecretKey {
+        val pbeKey = PBEKeySpec(phrase.toCharArray(), getRandomByteArray(16), 1000000, 256) // AES-256
+
+        return SecretKeySpec(
+            SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1").generateSecret(pbeKey).encoded,
+            "AES"
+        )
     }
 
     /**
@@ -30,14 +53,14 @@ object CryptoUtils {
      *
      * @param data 明文数据
      * @param secretKey 密钥
-     * @param iv 见[CryptoUtils.getRandomIv]
+     * @param iv 见[CryptoUtils.getRandomByteArray]
      * @return 密文数据
      */
     fun encrypt(data: ByteArray, secretKey: SecretKey, iv: ByteArray): ByteArray {
         val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
         val ivSpec = IvParameterSpec(iv)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec)
-        return cipher.doFinal(data)
+
+        return cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec).let { cipher.doFinal() }
     }
 
     /**
@@ -45,13 +68,13 @@ object CryptoUtils {
      *
      * @param data 密文数据
      * @param secretKey 密钥
-     * @param iv 见[CryptoUtils.getRandomIv]
+     * @param iv 见[CryptoUtils.getRandomByteArray]
      * @return 明文数据
      */
     fun decrypt(data: ByteArray, secretKey: SecretKey, iv: ByteArray): ByteArray {
         val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
         val ivSpec = IvParameterSpec(iv)
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec)
-        return cipher.doFinal(data)
+
+        return cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec).let { cipher.doFinal() }
     }
 }
