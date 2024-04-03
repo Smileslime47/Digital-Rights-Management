5b76380c0950885142cc0312ce0e08d513409c67
Smile_slime_47 <Smile_slime_47@outlook.com>
2024-04-03 17:01:48 +0800

    [blockchain]将注释从实现类转移到接口

--- drm-blockchain/src/main/kotlin/service/impl/AccountServiceImpl.kt
+++ drm-blockchain/src/main/kotlin/service/impl/AccountServiceImpl.kt
@@ -22,39 +22,15 @@ import java.math.BigDecimal
  * @author 刘一邦
  */
 class AccountServiceImpl : AccountService {
-    /**
-     * 通过密码和随机生成的密钥对创建WalletFile,并将KeyFile以WalletFile对象返回
-     *
-     * 由于KeyFile的特殊性，在考虑中心化存储该文件的情况下请考虑加密存储
-     * @see [CryptoUtils]
-     *
-     * @param password 新账户的密码
-     * @return KeyFile文件
-     */
     override fun newAccount(password: String): WalletFile =
         Wallet.createStandard(password, Keys.createEcKeyPair())
 
-    /**
-     * 通过BlockChain的Web3j实例创建一个新的区块链账户
-     *
-     * 注：不建议使用该接口，KeyFile会直接生成在区块链服务器的KeyStore文件夹下，导致账户难以管理
-     *
-     * @see [AccountServiceImpl.newAccount]
-     *
-     * @param password 新账户的密码
-     * @return 新账户的哈希地址（公钥的后二十位）
-     */
+    @Deprecated("Use newAccount instead")
     override fun newAccountByPersonal(password: String): String =
         if (!BlockChain.connected)
             throw BlockChainNotConnectedException("BlockChain not connected")
         else BlockChain.web3jInstance!!.personalNewAccount(password).send().result
 
-    /**
-     * 获取区块链指定账户余额
-     *
-     * @param addr 查询账户地址
-     * @return 账户余额（单位为Wei——1Wei = 1e-18 Eth）
-     */
     override fun getBalance(addr: String): BigDecimal =
         if (!BlockChain.connected)
             throw BlockChainNotConnectedException("BlockChain not connected")
@@ -64,15 +40,6 @@ class AccountServiceImpl : AccountService {
             )
         }
 
-
-    /**
-     * 通过银行账户向指定账户转账
-     * 调用[BlockChain.connect]时传入的WalletSource即为银行账户
-     *
-     * @param addr 收款账户地址
-     * @param value 收款金额
-     * @return 操作结果
-     */
     override fun chargeFromBank(addr: String, value: String): Boolean {
         try {
             Transfer(BlockChain.web3jInstance, BlockChain.bankTxManager)
@@ -83,14 +50,6 @@ class AccountServiceImpl : AccountService {
         }
     }
 
-    /**
-     * 获取指定区块链账户的TransactionManager
-     * 用于批准部署智能合约
-     *
-     * @param pwd 区块链账户密码
-     * @param keyFileContent 区块链账户的KeyFile文件内容，应当为序列化的Json字符串
-     * @return 指定账户的TransactionManager
-     */
     override fun getTxManager(pwd: String, keyFileContent: String): TransactionManager {
         val credentials = WalletUtils.loadJsonCredentials(pwd, keyFileContent)
         return RawTransactionManager(
